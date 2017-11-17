fcda3661c5bf3b569a77a7eb905ec44829c07037
cfib9 <cfib9@A3019-09>
2017-11-17 09:20:19 -0500

    9h20

--- Cours6app/Program.cs
+++ Cours6app/Program.cs
@@ -10,6 +10,16 @@ namespace Cours6app
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Quel est ton nom?");
+            String nom = Console.ReadLine();
+            Console.WriteLine("bonjour,{0}!", nom);
+
+            Console.WriteLine("entrez un nombre");
+            int nb1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entrez un autre nombre");
+            int nbr2 = int.Parse(Console.ReadLine());
+            int somme = nb1 / nbr2;
+            Console.WriteLine("la ration de {0} et {1} est {2}.", nb1, nbr2, somme);
         }
     }
 }
